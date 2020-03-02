70e4a05742c8e9f4d64f83aecddb8f2002ae52ee
CN-DXTZ <2924380441@qq.com>
2020-03-02 23:27:20 +0800

    TOC click to fold up or slide out

--- source/js/jquery.toc.js
+++ source/js/jquery.toc.js
@@ -1,17 +1,22 @@
-// TOC 滚动到固定位置开始固定
+var value_left = 0, isScrolled = false, isToTop = true; // 侧边点击标记变量
+
+// 滚动到固定位置开始固定
 var post_toc = document.getElementsByClassName("post-toc")[0]
-var toc_rest = document.documentElement.clientWidth * 7.5 / 100.0;
-window.addEventListener("scroll", e => {
-    var top = document.body.scrollTop || document.documentElement.scrollTop;
-    if (top > toc_rest) {
-        post_toc.style = "position:fixed;top:4.5vw;";
+var toc_ToTop = document.documentElement.clientWidth * 7 / 100.0;
+$(document).scroll(function () {
+    var top = $(document).scrollTop();
+    isScrolled = true;
+    if (top > toc_ToTop) {
+        isToTop = false;
+        post_toc.style = "position:fixed;top:5vw;left:-" + value_left + "vw;";
     } else {
-        post_toc.style = "";
+        isToTop = true;
+        post_toc.style = "position:reletive;left:-" + value_left + "vw;";
     }
 });
 
 
-// 对应目录小标题高亮
+// 对应目录标题高亮
 var $ol = $('.toc');
 var titleList = $('.post-content').find('h1,h2,h3,h4,h5,h6');
 $(window).scroll(function (e) {
@@ -33,3 +38,38 @@ $(window).scroll(function (e) {
         aList.addClass('actived');
     }
 });
+
+// 侧边点击收起滑出
+$(document).ready(function () {
+    var toc = $(".post-toc"),
+        title = $(".toc-title"),
+        icon = $(".icon-mulu"),
+        button = $(".icon-houtui"),
+        isShow = true;
+    title.click(function () {
+        if (isShow) {
+            isShow = false;
+            value_left = 18.1;
+            if (isToTop) {
+                post_toc.style = "position:reletive;left:-" + value_left + "vw;";
+            } else {
+                post_toc.style = "position:fixed;top:5vw;left:-" + value_left + "vw;";
+            }
+            icon.css("left", "9.4vw");
+            button.addClass("icon-qianjin");
+            title.css("transform", "translate\(5vw,0\)");
+
+        } else {
+            isShow = true;
+            value_left = 0;
+            icon.css("left", "0");
+            title.css("transform", "translate\(0,0\)");
+            button.removeClass("icon-qianjin");
+            if (isToTop) {
+                post_toc.style = "position:reletive;left:-" + value_left + "vw;";
+            } else {
+                post_toc.style = "position:fixed;top:5vw;left:-" + value_left + "vw;";
+            }
+        }
+    });
+})
