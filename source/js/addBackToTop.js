6a29f9bd0c15a105acf9bf437f49a5c8326a9e3c
CN-DXTZ <2924380441@qq.com>
2020-03-07 12:07:03 +0800

    diff-highlight

--- source/js/addBackToTop.js
+++ source/js/addBackToTop.js
@@ -1,5 +1,5 @@
 addBackToTop({
   diameter: 5 * Math.max(document.documentElement.clientWidth, document.documentElement.clientHeight) / 100.0,
-  backgroundColor: '#66BB6A',
+  backgroundColor: '#4CAF50',
   textColor: '#fff'
 })
